7a181a86ea5b6e0207af36fe208912bae95f997e
Ahmad Rianto <adrainusagi@gmail.com>
2026-03-18 09:26:15 +0700

    image process error debug

--- src/main/java/com/ituaku/image_service_api/controller/v1/ImageController.java
+++ src/main/java/com/ituaku/image_service_api/controller/v1/ImageController.java
@@ -234,7 +234,7 @@ public class ImageController {
                             .output(WebpWriter.DEFAULT, outputFile);
                     }
                 } catch (IOException e) {
-                    log.error("Failed to write WebP output to disk at path: {}", outputFile.getAbsolutePath(), e);
+                    log.error("Failed to write WebP output to disk at path: {}, error: {}", outputFile.getAbsolutePath(), e);
                     // Return a 500 error or throw a custom exception here
                     return ResponseEntity.internalServerError().build();
                 }
